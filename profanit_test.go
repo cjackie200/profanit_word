bca89654071dae5b5caf8e98ae8726703dd2bb52
cjackie200 <cjackie200@gmail.com>
2017-12-11 15:55:31 +0800

    Update profanit_test.go

--- profanit_test.go
+++ profanit_test.go
@@ -1,12 +1,10 @@
 package src
 
 import (
-	"bytes"
-	"bufio"
-	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
-	"testing"
-	"time"
+	"strings"
 )
 
 type Node struct {
@@ -17,13 +15,11 @@ type Node struct {
 func newNode() *Node {
 	return &Node{}
 }
-
-func (n *Node)checkAndFixMap(){
+func (n *Node) checkAndFixMap() {
 	if nil == n.child {
-		n.child=map[rune]*Node{}
+		n.child = map[rune]*Node{}
 	}
 }
-
 func (n *Node) setChild(key rune) *Node {
 	n.checkAndFixMap()
 	c, ok := n.child[key]
@@ -35,106 +31,99 @@ func (n *Node) setChild(key rune) *Node {
 		return node
 	}
 }
-
 func (n *Node) getChild(key rune) (*Node, bool) {
-	node, ok := n.child[key]
-	return node, ok
+	if n.child == nil || n == nil {
+		return nil, false
+	} else {
+		node, ok := n.child[key]
+		return node, ok
+	}
 }
-
 func (n *Node) setNodeEnd() {
 	n.end = true
 }
-
-func (n *Node) paserWord(word []rune) {
+func (n *Node) passerWord(word []rune) {
 	l := len(word)
 	next := n
 	for i := 0; i < l; i++ {
 		next = next.setChild(word[i])
-		// if next.end {
-		// 	return
-		// }
 	}
-	if(next != n){
+	if next != n {
 		next.setNodeEnd()
 	}
 }
-
 func (n *Node) checkWord(word []rune) bool {
 	l := len(word)
-	next := n
-	ok := false
+	p := n
 	for i := 0; i < l; i++ {
-		next, ok = next.getChild(word[i])
+		next, ok := p.getChild(word[i])
 		if ok {
 			if next.end {
-				return true
+				return false
+			} else {
+				p = next
 			}
 		} else {
-			return false
+			if p != n {
+				p = n
+				i--
+			}
 		}
 	}
-	return false
+	return true
 }
 
-type findWordStat struct{
+type findWordStat struct {
 	index [][2]int
-	pos int
-	mark int
+	pos   int
+	mark  int
 	isEnd bool
-	root *Node
+	root  *Node
 }
 
-func newFindWordStat(n *Node)*findWordStat{
+func newFindWordStat(n *Node) *findWordStat {
 	return &findWordStat{
-		index :[][2]int{},
-		mark:-1,
-		root:n,
+		index: [][2]int{},
+		mark:  -1,
+		root:  n,
 	}
 }
-
-func (self *findWordStat)setRoot(n *Node){
+func (self *findWordStat) setRoot(n *Node) {
 	self.root = n
 }
-func (self *findWordStat)setPos(i int){
+func (self *findWordStat) setPos(i int) {
 	self.pos = i
 }
-func (self *findWordStat)setMark(i int){
+func (self *findWordStat) setMark(i int) {
 	self.mark = i
 }
-
-func (self *findWordStat)flush(){
-	if self.mark >= self.pos{
+func (self *findWordStat) flush() {
+	if self.mark >= self.pos {
 		self.flushPushIndex()
-	}else{
+	} else {
 		self.flushNone()
 	}
 }
-
-func (self *findWordStat)flushNone(){
+func (self *findWordStat) flushNone() {
 	self.pos++
 	self.mark = -1
 }
-
-func (self *findWordStat)reFind(n *Node){
+func (self *findWordStat) reFind(n *Node) {
 	self.flush()
 	self.setRoot(n)
 }
-
-func (self *findWordStat)findNextNode(key rune) (*Node, bool){
+func (self *findWordStat) findNextNode(key rune) (*Node, bool) {
 	return self.root.getChild(key)
 }
-
-func (self *findWordStat)flushPushIndex(){
-	index := [2]int{self.pos,self.mark}
+func (self *findWordStat) flushPushIndex() {
+	index := [2]int{self.pos, self.mark}
 	self.index = append(self.index, index)
 	self.pos = self.mark + 1
 	self.mark = -1
 }
-
-func (self *findWordStat)getIndex() [][2]int{
+func (self *findWordStat) getIndex() [][2]int {
 	return self.index
 }
-
 func (n *Node) findWord(word []rune) [][2]int {
 	l := len(word)
 	finder := newFindWordStat(n)
@@ -156,12 +145,8 @@ func (n *Node) findWord(word []rune) [][2]int {
 	}
 	return finder.getIndex()
 }
-
-
-
 func replace(word []rune, mark rune) []rune {
 	result := root.findWord(word)
-	// fmt.Println(string(word), result)
 	if len(result) > 0 {
 		for _, v := range result {
 			for i := v[0]; i <= v[1]; i++ {
@@ -172,112 +157,40 @@ func replace(word []rune, mark rune) []rune {
 	return word
 }
 
-func (n *Node) Printf(str string) {
-	for k, v := range n.child {
-		if v.end {
-			v.Printf(str+"==>"+string([]rune{k})+"(end)")
-		}else{
-			v.Printf(str+"==>"+string([]rune{k}))
-		}
-		if v.child == nil {
-			fmt.Println(str + "==>" + string([]rune{k}) + "(end)")
-		}
-	}
-}
+var root = newNode()
 
-var data = [][]byte{
-	[]byte("123"),
-	[]byte("aba"),
-	[]byte("abc"),
-	[]byte("av123"),
-	[]byte("你好"),
-}
-
-var root = initRoot()
-var word = initWordData()
-
-func initWordData() map[int][]rune{
-	result := map[int][]rune{}
-	// fd, _ := os.Open("./words.txt")
-	fd, _ := os.Open("./2.txt")
-	defer fd.Close()
-	r := bufio.NewReader(fd)
-	i := 0
-	for {
-		data, err := readLine(r)
-		i++
-		if nil == err {
-			result[i] = data
-		} else {
-			return result
-		}
-	}
-}
-
-func initRoot() *Node{
-	node := newNode()
+func Configurate(fileName string) {
+	word := loadWord(fileName)
 	for _, data := range word {
-		node.paserWord(data)
-	}
-	return node
-}
-
-func readLine(r *bufio.Reader) ([]rune, error) {
-	line, err := r.ReadBytes('\n')
-	if err == nil {
-		data :=bytes.Runes(line[:len(line)-1])
-		return data, err
-	}else{
-		return []rune{}, err
+		root.passerWord([]rune(data))
 	}
 }
 
-
-
-func TestReplaceWord(t *testing.T) {
-	// root.Printf("root")
-	t1 := time.Now().UnixNano()
-	sum := 0
-	for _, v := range word {
-		src := string(v)
-		res := replace(v, '*')
-		// replace(v, '*')
-		fmt.Println("======")
-		fmt.Println(src)
-		fmt.Println(string(res))
-		if src == string(res) {
-			sum++
-			fmt.Println(src)
+func loadWord(filename string) []string {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0755)
+	defer file.Close()
+	if err != nil {
+		log.Panicln("Open UserID File, Error:", err.Error())
+	} else {
+		data, err := ioutil.ReadAll(file)
+		if nil != err {
+			log.Panicln("Read UserID File, Error:", err.Error())
 		}
+		return readFileLine(string(data))
 	}
-	runeStr:= bytes.Runes([]byte("男男女女哈淫水哈东亚病夫爱液啊"))
-	res := replace(runeStr, '*')
-	fmt.Println("男男女女哈淫水哈东亚病夫爱液啊")
-	fmt.Println(string(res))
-	t2 := time.Now().UnixNano()
-	fmt.Println("未过滤 :",sum, "总:", len(word), "耗时:", nsToMs(t2-t1),"ms")
+	return []string{}
 }
 
-func equalRune(a, b []rune) bool {
-	if len(a) != len(b){
-		return false
-	}else{
-		l := len(a)
-		for i := 0; i < l; i++ {
-			if a[i] != b[i] {
-				return false
-			}
-		}
-		return true
+func readFileLine(str string) []string {
+	win := strings.Split(str, "\r\n")
+	unix := strings.Split(str, "\n")
+	if len(win) == len(unix) {
+		return win
+	} else {
+		return unix
 	}
 }
 
-func nsToMs(ns int64) float64{
-	return float64(ns)/1000000.0
-}
-
-func BenchmarkReplaceWord(b *testing.B){
-	for i := 0; i < b.N; i++ {
-		replace(word[100], '*')
-	}
+func ValidWord(plain string) bool {
+	return root.checkWord([]rune(plain))
 }
